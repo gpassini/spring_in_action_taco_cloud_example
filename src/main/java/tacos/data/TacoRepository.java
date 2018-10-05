5086e248efde5094d2c8d7f8bce4894d1b01bb40
Gustavo Passini <gustavo.passini@gmail.com>
2018-10-04 20:47:53 -0400

    WIP Chapter 6.1.2

--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -1,8 +1,14 @@
 package tacos.data;
 
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
+
 import tacos.Taco;
 
 public interface TacoRepository extends CrudRepository<Taco, Long> {
+
+	List<Taco> findAll(PageRequest page);
 
 }
