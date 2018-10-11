988696bb9b45d3d65cbdd2f972406c622d35eda7
Gustavo Passini <gustavo.passini@gmail.com>
2018-10-11 19:56:17 -0400

    Chapter 6.2.3

--- src/main/java/tacos/web/rest/TacoResource.java
+++ src/main/java/tacos/web/rest/TacoResource.java
@@ -3,10 +3,12 @@ package tacos.web.rest;
 import java.util.Date;
 import java.util.List;
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.core.Relation;
 import lombok.Getter;
 import tacos.Taco;
 
 @Getter
+@Relation(value = "taco", collectionRelation = "tacos")
 public class TacoResource extends ResourceSupport {
 
   private static final IngredientResourceAssembler INGREDIENT_RESOURCE_ASSEMBLER =
